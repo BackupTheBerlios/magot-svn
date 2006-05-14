ee5b1c2c7f3b8554974f2896f18ad4f0294c62de
jdutreve <jdutreve@100f78ac-b8e9-0310-b54e-e4c10bb626f4>
2006-05-14 17:58:25 +0000

    version 0.1.1

--- trunk/setup.py
+++ trunk/setup.py
@@ -9,9 +9,9 @@ from setuptools import setup, find_packages
 setup(
     name = "Magot",
     version = "0.1.1",
-
+    
     description = "A cross-platform personal finance manager.",
-    author = "Jean-Philippe Dutrève",
+    author = "Jean-Philippe Dutreve",
     author_email = "jdutreve@users.berlios.de",
     url = "http://developer.berlios.de/projects/magot/",
 	download_url="http://download.berlios.de/magot/",
@@ -32,16 +32,16 @@ setup(
     packages = find_packages('src/py', exclude=['*.sandbox']),
     package_data = {'magot': ['*.ini']},
 
-    install_requires = ['PEAK>=0.5a3'],
+    install_requires = ['PEAK>=0.5a4'],
 
 	entry_points = {
         "console_scripts":["magot = magot.commands:runMain"]
     },
 	zip_safe = False,
 	
-    long_description = """\
-Magot is a cross-platform personal finance manager written in Python. A check-book like register GUI allows you to enter transactions and track bank accounts, incomes and expenses. It's based on formal accounting principles, PEAK and WxPython.
-""",
+    long_description = """Magot is a cross-platform personal finance manager written in Python. \
+    A check-book like register GUI allows you to enter transactions and track bank accounts, incomes and expenses. \
+    It's based on formal accounting principles, PEAK and WxPython.""",
     
-	keywords = "accounting,finance,Python,WxPython,PEAK,component,framework,agile",
+    keywords = "accounting, finance, Python, WxPython, PEAK, component, framework, agile",
 )
